03717c2768606b9c078ae4155d9e613e74c37795
Neda <neda.changizi@gmail.com>
2025-07-03 12:00:36 -0700

    refactor App component and add a variable for appName used in the welcome message

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "./components/Header";
 
 export default function App() {
+  const appName = "Summer 2025";
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Welcome to {appName}</Text>
       <StatusBar style="auto" />
+      <Header />
     </View>
   );
 }
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
